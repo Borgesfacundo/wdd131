70f1a5d1bc5ecb39dbdfe8628637e5396636dcb1
Borgesfacundo <borgesfacundo@gmail.com>
2024-06-11 11:39:11 -0300

    asdasd

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,9 @@ currentYear = document.querySelector("#currentyear");
 const today = new Date();
 
 currentYear.innerHTML = `${today.getFullYear()}`;
+
+lastMod = document.querySelector("#lastmodified");
+
+let oLastModif = new Date(document.lastModified);
+
+lastMod.innerHTML = `Last Modification: ${oLastModif}`;
