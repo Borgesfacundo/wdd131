73425f52ec2500ffe907e4411a0754aceb7bb414
Borgesfacundo <borgesfacundo@gmail.com>
2024-06-17 21:50:16 -0300

    Finishing week 02 assignment

--- scripts/temples.js
+++ scripts/temples.js
@@ -12,3 +12,12 @@ const lastMod = document.querySelector("#lastmodified");
 const text = `Last modified: ${lastModifElement}`;
 
 lastMod.textContent += text;
+
+//burger menu
+const navUl = document.querySelector(".nav");
+const hamburger = document.querySelector("#menuLink");
+
+hamburger.addEventListener("click", () => {
+    navUl.classList.toggle("show");
+    hamburger.classList.toggle("show");
+});
