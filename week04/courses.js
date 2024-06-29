40b61f59fc983f52dd892636f062ecb0047c0543
Borgesfacundo <borgesfacundo@gmail.com>
2024-06-29 11:33:36 -0300

    Code working courses activity

---
+++ week04/courses.js
@@ -0,0 +1,70 @@
+const aCourse = {
+    code: "CSE121b",
+    name: "Javascript Language",
+    sections: [{
+        sectionNum: 1,
+        roomNum: 'STC 353',
+        enrolled: 26,
+        days: 'TTh',
+        instructor: 'Bro T'
+    },
+    {
+        sectionNum: 2,
+        roomNum: 'STC 347',
+        enrolled: 28,
+        days: 'TTh',
+        instructor: 'Sis A'
+    }],
+    enrollStudent: function (sectionNum) {
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled++;
+            renderSections(this.sections);
+        }
+    },
+    dropStudent: function (sectionNum) {
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled--;
+            renderSections(this.sections);
+        }
+    }
+};
+
+//Function to add name and course number
+function addCourseNameandCode(object) {
+    document.getElementById("courseName").textContent = object.name;
+    document.getElementById("courseCode").textContent = this.code;
+}
+
+//Output sections
+
+function templateSection(section) {
+    return `<tr>
+    <td>${section.sectionNum}</td>
+    <td>${section.roomNum}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.days}</td>
+    <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+    const html = sections.map(templateSection);
+    document.querySelector("#sections").innerHTML = html.join("");
+}
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
+document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.dropStudent(sectionNum);
+});
+
+addCourseNameandCode(aCourse);
+renderSections(aCourse.sections);
