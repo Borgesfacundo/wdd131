13cc36d1709051e592c3959ffbef3ef0ea70da1d
Borgesfacundo <bor22016@byui.edu>
2024-07-18 20:58:48 -0600

    asdadsad

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -7,5 +7,23 @@ const dateFunction = () => {
 
 window.onload = dateFunction;
 
+const visits = document.querySelector(".num-of-visits");
+
+let countOfVisits = () => {
+let visitCount = localStorage.getItem("visitsCount");
+
+if (visitCount === null) {
+    visitCount = 0;
+} else {
+    visitCount = parseInt(visitCount, 10);
+}
+
+visitCount += 1;
+localStorage.setItem("visitsCount", visitCount);
+
+visits.innerHTML = `This is your visit number ${visitCount}!`;
+}
+
+countOfVisits();
 
 
