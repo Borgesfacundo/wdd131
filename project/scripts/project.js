c6c99180f8ec2b7e4872801fe80f0aa4cad91fee
Borgesfacundo <bor22016@byui.edu>
2024-07-18 20:11:24 -0600

    asdasd

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,23 +1,4 @@
-const bmiElement = document.querySelector(".bmi-result");
-const button = document.querySelector(".bmi-button");
 const currentYear = document.querySelector(".currentyear");
-
-
-document.getElementById('bmi-form').addEventListener('submit', function (event) {
-    event.preventDefault()
-
-    let height = parseFloat(document.querySelector("#height").value);
-    let weigth = parseFloat(document.querySelector("#weight").value);
-    bmiCalculator(weigth, height)
-
-});
-
-function bmiCalculator(weight, height) {
-    height = height / 100
-    let bmiResult = weight / (height * height)
-    bmiElement.innerHTML = `<b>Your BMI is ${bmiResult.toFixed(2)}</b>`;
-}
-
 
 const dateFunction = () => {
     const date = new Date();
